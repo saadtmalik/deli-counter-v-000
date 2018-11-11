55229eeaa8a88e66985f7138be2df0b9e5febce3
Saad Malik <saad.t.malik@gmail.com>
2018-11-11 08:50:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,23 @@
 def take_a_number(katz_deli, name)
+  name.each do |position|
+    
 
 end
 
-def line(katz_deli)
-  if katz_deli = []
+def line(array)
+  if array.length == 0
     puts "This line is currently empty."
-  elsif katz_deli != []
-    visit
+  elsif 
+    return "The line is currently:"
+    array.each_with_index do |value, index|
+      
+  
+      
   end
 end
 
 # line method shows everyones current place in line or says "this line is empty"
+
 
 def now_serving(katz_deli)
 
