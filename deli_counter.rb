595a47630d7768893904a32e096211fb8682e74a
Saad Malik <saad.t.malik@gmail.com>
2018-11-11 08:28:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli = []
+    return "This line is currently empty."
+  elsif katz_deli != []
+  return blank
+end
+end
+
+def take_a_number(katz_deli, name)
+
+end
+
+def now_serving(katz_deli)
+
+end
