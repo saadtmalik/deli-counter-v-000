9634d8441546da7576e171d0db5a164a31b4ca03
Saad Malik <saad.t.malik@gmail.com>
2018-11-11 08:28:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@ def line(katz_deli)
   if katz_deli = []
     return "This line is currently empty."
   elsif katz_deli != []
-  return blank
-end
+    return blank
+  end
 end
 
 def take_a_number(katz_deli, name)
